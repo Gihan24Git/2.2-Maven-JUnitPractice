97a47bd4fdbee2c7434989bba792ce210ca59433
Gihan24Git <gihan.cullen24@gmail.com>
2026-01-14 16:18:17 +0530

    Get student details and print through getters.

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,43 +1,13 @@
 package org.example;
 
+import lombok.Data;
 import java.util.List;
 
+@Data
 public class Student {
 
     private int id;
     private String name;
     private int age;
     private List<String> subjects;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public List<String> getSubjects() {
-        return subjects;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setSubjects(List<String> subjects) {
-        this.subjects = subjects;
-    }
 }

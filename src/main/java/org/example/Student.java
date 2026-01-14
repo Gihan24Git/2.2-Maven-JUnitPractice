b28b81404b60b8f4f28df6e561d3b8903e130896
Gihan24Git <gihan.cullen24@gmail.com>
2026-01-14 15:24:52 +0530

    Print student details using getters

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -9,42 +9,32 @@ public class Student {
     private int age;
     private List<String> subjects;
 
-    public Student() {
-    }
-
-    public Student(int id, String name, int age, List<String> subjects) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.subjects = subjects;
-    }
-
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public List<String> getSubjects() {
         return subjects;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void setSubjects(List<String> subjects) {
